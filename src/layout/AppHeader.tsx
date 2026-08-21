54fc3ccf890500e004e7fdbc82f96a1ed99057f9
Peeyush Budhia <peeyush.budhia@gmail.com>
2026-08-21 09:07:51 +0000

    chore: remove unused frontend assets

--- src/layout/AppHeader.tsx
+++ src/layout/AppHeader.tsx
@@ -85,12 +85,12 @@ const AppHeader: React.FC = () => {
           <Link to="/" className="lg:hidden">
             <img
               className="dark:hidden"
-              src="./images/logo/logo.svg"
+              src="./images/logo/laravel-api-base-horizontal.png"
               alt="Logo"
             />
             <img
               className="hidden dark:block"
-              src="./images/logo/logo-dark.svg"
+              src="./images/logo/laravel-api-base-horizontal.png"
               alt="Logo"
             />
           </Link>
